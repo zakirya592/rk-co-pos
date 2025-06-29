17da6e915cb17bcb73c48780fe7541fdc0f517f0
zakirya592 <muhammadzakirya592@gmail.com>
2025-06-29 11:05:03 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,7 @@ function AppContent() {
 
   return (
     <Routes>
+      <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />} />
       {/* Protected routes */}
       {user && (
