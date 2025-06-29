1e9e4497a489814f6336d41dc12c9cd912a68898
zakirya592 <muhammadzakirya592@gmail.com>
2025-06-29 14:04:23 +0500

    updated

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,7 +5,7 @@ import Products from '../pages/Product/Products';
 import Categories from '../pages/Categories/Categories';
 import Reports from './Reports';
 import History from './History';
-import POS from './POS';
+import POS from '../pages/POS/POS';
 import Customers from './Customers';
 import Settings from './Settings';
 import User from '../pages/User/User';
