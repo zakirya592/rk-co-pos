15f08b6e37d5c4b0c8fae3b3bb19b4062033dd3c
zakirya592 <muhammadzakirya592@gmail.com>
2025-07-03 12:53:35 +0500

    product journeys filter

--- src/pages/Product/ProductJourney.jsx
+++ src/pages/Product/ProductJourney.jsx
@@ -56,7 +56,11 @@ const ProductJourney = () => {
               </span>
             }
           >
-            {filteredJourneys.length} Journeys
+            {
+              filteredJourneys.filter((journey) => journey.action === "updated")
+                .length
+            }{" "}
+            Journeys
           </Chip>
         </div>
       </div>
@@ -79,148 +83,164 @@ const ProductJourney = () => {
         <div className="flex justify-center items-center py-8">
           <Spinner color="success" size="lg" />
         </div>
+      ) : journeys.length === 0 ? (
+        <div className="text-center text-gray-500 py-8">
+          <p className="text-lg">No journeys found</p>
+        </div>
       ) : filteredJourneys.length === 0 ? (
-        <div className="text-center text-gray-500 py-8">No journeys found</div>
+        <div className="text-center text-gray-500 py-8">
+          <div className="flex flex-col items-center gap-4">
+            <FaSearch className="text-gray-400 text-4xl" />
+            <p className="text-lg">No journeys found</p>
+            <p className="text-gray-500">
+              Try searching again with different keywords
+            </p>
+          </div>
+        </div>
       ) : (
         <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-6">
-          {filteredJourneys.map((journey) => (
-            <Card
-              key={journey._id}
-              className="hover:shadow-lg transition-shadow"
-            >
-              <CardBody className="p-6">
-                <div className="flex flex-col gap-4">
-                  <div className="flex items-center justify-between">
-                    <div className="flex items-center gap-3">
-                      <h4 className="text-lg font-semibold text-gray-800">
-                        Product Name :
-                      </h4>
-                      <h3 className="text-lg">
-                        {journey?.product?.name || "N/A"}
-                      </h3>
-                    </div>
-                  </div>
-
-                  <div>
-                    <div className="flex flex-col sm:flex-row gap-2 justify-between">
-                      <p className="text-gray-600">
-                        <span className="text-lg font-semibold text-gray-800">
-                          Date:
-                        </span>{" "}
-                        {new Date(journey.createdAt).toLocaleDateString()}
-                      </p>
-                      <Chip
-                        color={
-                          journey.action === "created" ? "success" : "warning"
-                        }
-                        variant="flat"
-                      >
-                        {journey.action}
-                      </Chip>
-                    </div>
-                    {/* Changes Section */}
-                    {journey.changes && journey.changes.length > 0 && (
-                      <div className="mt-4 space-y-3">
+          {filteredJourneys
+            .filter((journey) => journey.action === "updated")
+            .map((journey) => (
+              <Card
+                key={journey._id}
+                className="hover:shadow-lg transition-shadow"
+              >
+                <CardBody className="p-6">
+                  <div className="flex flex-col gap-4">
+                    <div className="flex items-center justify-between">
+                      <div className="flex items-center gap-3">
                         <h4 className="text-lg font-semibold text-gray-800">
-                          Changes Made
+                          Product Name :
                         </h4>
-                        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6">
-                          {journey.changes.map((change, index) => (
-                            <Card
-                              key={index}
-                              className="bg-gradient-to-r from-gray-50 to-white"
-                            >
-                              <CardBody className="p-4">
-                                <div className="flex flex-col gap-2">
-                                  <p className="text-sm font-medium text-gray-700">
-                                    <span className="text-gray-500">
-                                      Field:
-                                    </span>{" "}
-                                    {change.field}
-                                  </p>
-                                  <div className="flex items-center gap-2">
-                                    {change.field
-                                      .toLowerCase()
-                                      .includes("image") ? (
-                                      <div className="flex flex-row gap-2">
-                                        <div className="flex items-center gap-2 ">
-                                          <span className="text-gray-500 text-xs">
-                                            Old:
-                                          </span>
-                                          {(change.oldValue && (
-                                            <img
-                                              src={change.oldValue}
-                                              alt="Old Image"
-                                              className="w-16 h-16 object-cover rounded-lg border border-gray-200"
-                                            />
-                                          )) || (
-                                            <div className="w-16 h-16 rounded-lg border border-gray-200 bg-gray-50 flex items-center justify-center">
-                                              <span className="text-gray-300 text-xs">
-                                                Empty
-                                              </span>
-                                            </div>
-                                          )}
-                                        </div>
-                                        <span className="text-gray-500 my-auto">
-                                          →
-                                        </span>
-                                        <div className="flex items-center gap-2">
-                                          <span className="text-green-500 text-xs">
-                                            New:
-                                          </span>
-                                          {(change.newValue && (
-                                            <img
-                                              src={change.newValue}
-                                              alt="New Image"
-                                              className="w-16 h-16 object-cover rounded-lg border border-gray-200"
-                                            />
-                                          )) || (
-                                            <span className="text-gray-500 text-xs">
-                                              N/A
-                                            </span>
-                                          )}
-                                        </div>
-                                      </div>
-                                    ) : (
-                                      <>
-                                        <Chip
-                                          color="warning"
-                                          variant="flat"
-                                          className="text-xs"
-                                        >
-                                          Old: {change.oldValue || "N/A"}
-                                        </Chip>
-                                        <span className="text-gray-500">→</span>
-                                        <Chip
-                                          color="success"
-                                          variant="flat"
-                                          className="text-xs"
-                                        >
-                                          New: {change.newValue || "N/A"}
-                                        </Chip>
-                                      </>
-                                    )}
-                                  </div>
-                                </div>
-                              </CardBody>
-                            </Card>
-                          ))}
-                        </div>
+                        <h3 className="text-lg">
+                          {journey?.product?.name || "N/A"}
+                        </h3>
                       </div>
-                    )}
+                    </div>
 
-                    <p className="text-gray-600">
-                      {" "}
-                      <span className="text-lg font-semibold text-gray-800">
-                        Notes:
-                      </span>{" "}
-                      {journey.notes}
-                    </p>
+                    <div>
+                      <div className="flex flex-col sm:flex-row gap-2 justify-between">
+                        <p className="text-gray-600">
+                          <span className="text-lg font-semibold text-gray-800">
+                            Date:
+                          </span>{" "}
+                          {new Date(journey.updatedAt).toLocaleDateString()}
+                        </p>
+                        <Chip
+                          color={
+                            journey.action === "created" ? "success" : "warning"
+                          }
+                          variant="flat"
+                        >
+                          {journey.action}
+                        </Chip>
+                      </div>
+                      {/* Changes Section */}
+                      {journey.changes && journey.changes.length > 0 && (
+                        <div className="mt-4 space-y-3">
+                          <h4 className="text-lg font-semibold text-gray-800">
+                            Changes Made
+                          </h4>
+                          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6">
+                            {journey.changes.map((change, index) => (
+                              <Card
+                                key={index}
+                                className="bg-gradient-to-r from-gray-50 to-white"
+                              >
+                                <CardBody className="p-4">
+                                  <div className="flex flex-col gap-2">
+                                    <p className="text-sm font-medium text-gray-700">
+                                      <span className="text-gray-500">
+                                        Field:
+                                      </span>{" "}
+                                      {change.field}
+                                    </p>
+                                    <div className="flex items-center gap-2">
+                                      {change.field
+                                        .toLowerCase()
+                                        .includes("image") ? (
+                                        <div className="flex flex-row gap-2">
+                                          <div className="flex items-center gap-2 ">
+                                            <span className="text-gray-500 text-xs">
+                                              Old:
+                                            </span>
+                                            {(change.oldValue && (
+                                              <img
+                                                src={change.oldValue}
+                                                alt="Old Image"
+                                                className="w-16 h-16 object-cover rounded-lg border border-gray-200"
+                                              />
+                                            )) || (
+                                              <div className="w-16 h-16 rounded-lg border border-gray-200 bg-gray-50 flex items-center justify-center">
+                                                <span className="text-gray-300 text-xs">
+                                                  Empty
+                                                </span>
+                                              </div>
+                                            )}
+                                          </div>
+                                          <span className="text-gray-500 my-auto">
+                                            →
+                                          </span>
+                                          <div className="flex items-center gap-2">
+                                            <span className="text-green-500 text-xs">
+                                              New:
+                                            </span>
+                                            {(change.newValue && (
+                                              <img
+                                                src={change.newValue}
+                                                alt="New Image"
+                                                className="w-16 h-16 object-cover rounded-lg border border-gray-200"
+                                              />
+                                            )) || (
+                                              <span className="text-gray-500 text-xs">
+                                                N/A
+                                              </span>
+                                            )}
+                                          </div>
+                                        </div>
+                                      ) : (
+                                        <>
+                                          <Chip
+                                            color="warning"
+                                            variant="flat"
+                                            className="text-xs"
+                                          >
+                                            Old: {change.oldValue || "N/A"}
+                                          </Chip>
+                                          <span className="text-gray-500">
+                                            →
+                                          </span>
+                                          <Chip
+                                            color="success"
+                                            variant="flat"
+                                            className="text-xs"
+                                          >
+                                            New: {change.newValue || "N/A"}
+                                          </Chip>
+                                        </>
+                                      )}
+                                    </div>
+                                  </div>
+                                </CardBody>
+                              </Card>
+                            ))}
+                          </div>
+                        </div>
+                      )}
+
+                      <p className="text-gray-600">
+                        {" "}
+                        <span className="text-lg font-semibold text-gray-800">
+                          Notes:
+                        </span>{" "}
+                        {journey.notes}
+                      </p>
+                    </div>
                   </div>
-                </div>
-              </CardBody>
-            </Card>
-          ))}
+                </CardBody>
+              </Card>
+            ))}
         </div>
       )}
     </div>
